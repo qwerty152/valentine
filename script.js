10bbeaea051de1cf0048f854b592494b127c091e
maxim <kotm7175@gmail.com>
2026-02-14 11:56:26 +0300

    fd

--- script.js
+++ script.js
@@ -54,16 +54,16 @@
 })();
 */
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Ты уверена?",
+    "Серьезно??",
+    "Точно?",
+    "Ну пожалуйста...",
+    "Подумай еще!",
+    "Если скажешь нет я буду грустным :(",
+    "Очень грустным",
+    "Прям как Новик...",
+    "Да ты че, гонишь что-ли",
+    "Шучу, скажи да, пожалуйста! ❤️"
 ];
 
 let messageIndex = 0;
